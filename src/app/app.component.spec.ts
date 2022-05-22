77cd0a381f715e7b908fce5e7ff6c6157a658803
karthick-0904 <m.karthick9400@gmail.com>
2022-05-22 18:02:56 +0530

    new commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'B19BETA'`, () => {
+  it(`should have as title 'sample'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('B19BETA');
+    expect(app.title).toEqual('sample');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('B19BETA app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sample app is running!');
   });
 });
